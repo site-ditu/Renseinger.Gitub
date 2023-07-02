97f7d7f4dae50199d6605501fe1b467aa17cda46
etude <135482476+site-ditu@users.noreply.github.com>
2023-07-02 18:04:48 +0000

    Create ama.sql

---
+++ ama.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE etude;
+USE etude;
+CREATE TABLE eleve (
+    idEleve smallint UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nomEleve VARCHAR(25),
+    PrenomEleve VARCHAR(25),
+    numeroEleve INT
+    emailEleve VARCHAR(255),
+    motdepassEleve TEST,
+
